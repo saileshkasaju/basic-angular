353c72db907c48d50a09350b2cd818a681a38cb7
saileshkasaju <leosailesh@gmail.com>
2017-03-11 12:45:42 +0545

    made a basic contoller and display basic user information from github api

---
+++ script.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Edge on 3/11/2017.
+ */
+
+var MainController = function($scope, $http) {
+
+    var onUserComplete = function(response) {
+        $scope.user = response.data;
+    };
+
+    var onError = function(reason) {
+        $scope.error = "Could not fetch the user";
+    };
+
+    $http.get("https://api.github.com/users/robconery")
+        .then(onUserComplete, onError);
+
+    $scope.message = "Hello, Angular!";
+};
