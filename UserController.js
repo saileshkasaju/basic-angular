8b38b3611f25fff32fa0f66900fa7c7b43b2fa3e
saileshkasaju <leosailesh@gmail.com>
2017-03-15 21:45:20 +0545

    Updates

---
+++ UserController.js
@@ -0,0 +1,20 @@
+(function(){
+    var app = angular.module("githubViewer");
+    var UserController = function($scope, github, $routeParams) {
+        var onUserComplete = function(data) {
+            $scope.user = data;
+            github.getRepos($scope.user)
+                .then(onRepos, onError);
+        };
+        var onRepos = function (data) {
+            $scope.repos = data;
+        };
+        var onError = function(reason) {
+            $scope.error = "Could not fetch the data";
+        };
+        $scope.username= $routeParams.username;
+        $scope.message = "GitHub Viewer";
+        github.getUser($scope.username).then(onUserComplete, onError);
+    };
+    app.controller("UserController", UserController);
+}());
